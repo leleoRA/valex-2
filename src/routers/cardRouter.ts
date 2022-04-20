b3cae98b8f5f7938ce5b70de8146eb34acb5bfa4
Celso <celsolnv27@gmail.com>
2022-04-19 23:47:20 -0300

    Preparando revisão de código

--- src/routers/cardRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as cardController from '../controllers/cardController.js';
-import { validateAPImiddleware } from "../middlewares/validateAPImiddleware.js";
-
-const cardRouter=Router();
-
-cardRouter.post('/createcard',validateAPImiddleware,cardController.createCard);
-
-export default cardRouter;
